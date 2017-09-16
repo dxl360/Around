d191c1f7381c9dd90bea605495bdede1ebd3e73e
duanli <dxl360@case.edu>
2017-09-16 16:32:16 -0700

    change server

--- service/main.go
+++ service/main.go
@@ -32,7 +32,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.163.135.7:9200"
+	ES_URL = "http://52.43.84.96:9200"
 )
 
 
